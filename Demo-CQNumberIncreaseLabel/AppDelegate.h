5924e343a7a8d744d3efe9bee59c6b13d2c8a4c9
caiqiang <caiqiang@kuaijiankang.com>
2017-06-24 15:22:36 +0800

    Initial Commit

---
+++ Demo-CQNumberIncreaseLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo-CQNumberIncreaseLabel
+//
+//  Created by 蔡强 on 2017/6/24.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
